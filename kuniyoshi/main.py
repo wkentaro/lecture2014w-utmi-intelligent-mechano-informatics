db539afa0bfe8e40e951f1993986cfcb7ba3caf1
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-07-22 01:20:18 +0900

    split main to recall.py

--- kuniyoshi/main.py
+++ kuniyoshi/main.py
@@ -11,6 +11,11 @@ import click
 
 from utils import binarize
 from fit import fit_hopfield
+from recall import (
+    recall_with_noise,
+    get_recalling_performance,
+    view_recalling_result,
+    )
 from visualization import (
     print_header,
     print_params,
@@ -35,47 +40,25 @@ def main(n_sample, n_label, noise_amount, fit_mode, save_fig):
     print_params(**params)
 
     # fit hopfield
-    print_header('fitting hopfield')
+    print('\n.. fitting hopfield\n')
     hf, X, y, target_names, params = fit_hopfield(params)
     print_params(**params)
 
     # recall
-    X_noise = random_noise(X.astype(float), mode='s&p', amount=noise_amount)
-    X_noise = binarize(X_noise, binary_values=(-1,1))
-    X_recall = []
-    for x in X_noise:
-        recall = hf.recall(x=x, n_times=10).reshape(-1)
-        recall[recall < 0] = -1
-        recall[recall >= 0] = 1
-        X_recall.append(recall)
-    X_recall = np.array(X_recall)
+    print('\n.. recalling\n')
+    X, X_noise, X_recall = recall_with_noise(clf=hf, X=X,
+                                             noise_amount=noise_amount)
 
     print_header('result')
-    # similarity
-    similarity = np.array([np.linalg.norm(o-r) for o, r in zip(X, X_recall)])
-    similarity /= X[0].size
-    similarity = 1. - similarity
-    print('similarity:', np.mean(similarity))
-    # accuracy
-    mask = (similarity == 1)
-    accuracy = np.copy(similarity)
-    accuracy[mask], accuracy[~mask] = 1, 0
-    print('accuracy:', accuracy.sum() / len(accuracy))
+    similarities, accurate = get_recalling_performance(X, X_recall)
+    print('similarity:', np.mean(similarities))
+    print('accuracy:', np.mean(accurate))
 
     # compare 3 images & save
     if save_fig:
-        h, w = img_shape
-        X = X.reshape((-1, h, w)).astype(int)
-        X_noise = X_noise.reshape((-1, h, w)).astype(int)
-        X_recall = X_recall.reshape((-1, h, w)).astype(int)
-        for org, noise, recall, a in zip(X, X_noise, X_recall, accuracy):
-            if a == 1:
-                save_dir = 'accurate_{}'.format(n_label)
-            elif a == 0:
-                save_dir = 'wrong_{}'.format(n_label)
-            else:
-                raise ValueError('unnexpected accuracy value')
-            compare_origin_noise_recall(org, noise, recall, save_dir)
+        print('\n.. view recalling result\n')
+        view_recalling_result(X, X_noise, X_recall,
+                              accurate=accurate, **params)
 
 
 if __name__ == '__main__':
---
+++ kuniyoshi/recall.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+from skimage.util import random_noise
+
+from utils import binarize
+from visualization import compare_origin_noise_recall
+
+
+def recall_with_noise(clf, X, noise_amount=0.05):
+    X = X.astype(float)
+    X_noise = random_noise(X, mode='s&p', amount=noise_amount)
+    X_noise = binarize(X_noise, binary_values=(-1,1))
+    X_recall = []
+    for x in X_noise:
+        recall = clf.recall(x=x, n_times=10).reshape(-1)
+        recall[recall < 0] = -1
+        recall[recall >= 0] = 1
+        X_recall.append(recall)
+    X_recall = np.array(X_recall)
+    return X, X_noise, X_recall
+
+
+def get_recalling_performance(X, X_recall):
+    # similarity
+    similarities = np.array([np.linalg.norm(o-r) for o, r in zip(X, X_recall)])
+    similarities /= X[0].size
+    similarities = 1. - similarities
+    # accuracy
+    mask = (similarities == 1)
+    accurate = np.copy(similarities)
+    accurate[mask], accurate[~mask] = 1, 0
+    return similarities, accurate
+
+
+def view_recalling_result(X, X_noise, X_recall, accurate, **kwargs):
+    # get params
+    n_label = kwargs['n_label']
+    img_shape = kwargs['img_shape']
+
+    h, w = img_shape
+    X = X.reshape((-1, h, w)).astype(int)
+    X_noise = X_noise.reshape((-1, h, w)).astype(int)
+    X_recall = X_recall.reshape((-1, h, w)).astype(int)
+    for org, noise, recall, a in zip(X, X_noise, X_recall, accurate):
+        if a == 1:
+            save_dir = 'accurate_{}'.format(n_label)
+        elif a == 0:
+            save_dir = 'wrong_{}'.format(n_label)
+        else:
+            raise ValueError('unnexpected accuracy value')
+        compare_origin_noise_recall(org, noise, recall, save_dir)
