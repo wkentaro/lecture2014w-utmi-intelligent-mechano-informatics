99f8b7b5d14a00b9a970f2358cba0eb8c7fe1066
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-07-21 19:22:58 +0900

    visualize changes

--- kuniyoshi/main.py
+++ kuniyoshi/main.py
@@ -7,6 +7,7 @@ import numpy as np
 from skimage.util import random_noise
 from skimage.transform import resize
 import matplotlib.pyplot as plt
+import click
 
 from hopfield import Hopfield
 from utils import binarize
@@ -14,12 +15,13 @@ from data import load_alphabet, create_train_data
 from visualization import print_header, print_train_data, print_params
 
 
-def main():
+@click.command()
+@click.option('--n-label', default=2, type=int, help='number of labels')
+def main(n_label):
     # load dataset
     dataset = load_alphabet()
 
     # parameters
-    n_label = 2
     img_shape = dataset.image_shape
     print_params(n_label=n_label, img_shape=img_shape)
 
@@ -40,28 +42,17 @@ def main():
 
     # recall
     index = np.random.randint(0, len(X))
-    print_header('input')
-    img = X[index].reshape(img_shape).astype(int)
-    img[img == -1] = 0
-    print(img)
-    plt.subplot(131)
-    plt.imshow(img, cmap='gray')
-    print_header('input_with_noise')
-    input_ = X[index].reshape(img_shape)
-    input_ = random_noise(X[index].astype(float), mode='s&p', amount=0.1)
-    input_ = binarize(input_, binary_values=(-1,1))
-    img = input_.reshape(img_shape).astype(int)
-    img[img == -1] = 0
-    print(img)
-    plt.subplot(132)
-    plt.imshow(img, cmap='gray')
-    print_header('output')
-    ret = hf.recall(x=input_, n_times=10)
-    img = ret.reshape(img_shape).astype(int)
-    img[img == -1] = 0
-    print(img)
-    plt.subplot(133)
-    plt.imshow(img, cmap='gray')
+    origin = X[index]
+    noise = random_noise(origin.astype(float), mode='s&p', amount=0.1)
+    noise = binarize(noise, binary_values=(-1,1))
+    recall = hf.recall(x=noise, n_times=10)
+    imgs = map(lambda x:x.reshape(img_shape).astype(int),
+               [origin, noise, recall])
+    for i, img in enumerate(imgs):
+        plt.subplot(131 + i)
+        plt.imshow(img, cmap='gray')
+        print(img.astype(int))
+    print('norm:', np.linalg.norm(imgs[0] - imgs[2]))
     plt.show()
 
 
